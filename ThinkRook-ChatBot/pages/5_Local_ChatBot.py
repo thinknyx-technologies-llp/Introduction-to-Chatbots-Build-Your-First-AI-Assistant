728a7e0f7bf956cee10328537839b63b6c3e2d49
dheeraj-sain <dheeraj.sain@thinknyx.com>
2025-07-14 12:26:53 +0530

    update: Local Chatbot Application.

--- ThinkRook-ChatBot/pages/5_Local_ChatBot.py
+++ ThinkRook-ChatBot/pages/5_Local_ChatBot.py
@@ -26,9 +26,10 @@ if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
-    with st.spinner("Bot is Thinking....."):
+    with st.spinner("Bot is Thinking..."):
         response = ollama.chat(model = model, messages = st.session_state.messages)
         answer = response['message']['content']
 
     st.session_state.messages.append({"role": "assistant", "content": answer})
     st.chat_message("assistant").write(answer)
+    
